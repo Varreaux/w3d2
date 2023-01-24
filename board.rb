b56ae10be4c0abd09b8c5a1a73cc7367922a54bf
Olga Borovikova <olga.sleepless@gmail.com>
2023-01-24 14:44:13 -0500

    generate_characters method

--- board.rb
+++ board.rb
@@ -15,9 +15,9 @@ class Board
         @grid[row][col] = val
     end
 
-
-    def populate
-        numberofcards = (n*n)/2
+    # generate_characters generate n different letters from the alphabet
+    def generate_characters
+        numberofcards = (@grid.length*@grid.length)/2
         alpha = *('a'..'z') 
         alpha.sample
         characters = []
@@ -26,6 +26,25 @@ class Board
             char = alpha.sample 
             characters << char if !characters.include?(char)
         end
+
+        return characters.concat(characters)
+    end
+
+    def populate
+        characters = self.generate_characters
+        rows = *(0..@grid.length-1)
+        cols = *(0..@grid.length-1)
+
+        characters.each do |letter|
+            row = rows.sample
+            col = cols.sample
+            @grid[row][col] = letter if !@grid[row][col]
+
+        end
+        
+    end
+    
+    
 
         
 
